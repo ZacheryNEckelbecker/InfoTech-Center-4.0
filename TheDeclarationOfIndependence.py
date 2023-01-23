6ded947cb8879b3f89fb00e1568dcc7a1f3c7c78
Zachery N Eckelbecker <02Zachery.eckelbecke@gulllakecs.org>
2023-01-23 13:49:53 -0500

    Update welcome message

--- TheDeclarationOfIndependence.py
+++ TheDeclarationOfIndependence.py
@@ -1,3 +1,9 @@
 # Programmer : Zachery Eckelbecker
 # Date: 1.28.2023
 # program: Infotech Center Upgrades
+
+"""
+Our welcome screen will start our program letting drivers
+know that the InfoTech Center 4.0 OS is loading
+"""
+print ("\n\nWelcome - InfoTech 4.0 OS is Loading")
