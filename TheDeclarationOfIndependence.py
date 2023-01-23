7d1dfda9c741a80c1c84a3aa0af76c72e1ffc995
Zachery N Eckelbecker <02Zachery.eckelbecke@gulllakecs.org>
2023-01-23 14:13:47 -0500

    Added sleep library

--- TheDeclarationOfIndependence.py
+++ TheDeclarationOfIndependence.py
@@ -6,4 +6,12 @@
 Our welcome screen will start our program letting drivers
 know that the InfoTech Center 4.0 OS is loading
 """
+# Import Libraries Here
+from time import sleep
+
+
+
 print ("\n\nWelcome - InfoTech 4.0 OS is Loading")
+sleep(1)
+
+
