b06fb1b52eec960d4a08bd5a068a00cd5d4a5c0b
Zachery N Eckelbecker <02Zachery.eckelbecke@gulllakecs.org>
2023-01-24 14:09:21 -0500

    Added while statement

--- TheDeclarationOfIndependence.py
+++ TheDeclarationOfIndependence.py
@@ -1,3 +1,26 @@
 # Programmer : Zachery Eckelbecker
 # Date: 1.28.2023
 # program: Infotech Center Upgrades
+
+"""
+Our welcome screen will start our program letting drivers
+know that the InfoTech Center 4.0 OS is loading
+"""
+# Import Libraries Here
+import time
+import sys
+
+
+x = 0
+a = 0
+
+while x != 20:
+    x += 1
+    b = ("\033[\Welcome - InfoTech 4.0 OS is Loading" + "." * a)
+    a = a + 1
+    sys.stdout.write('\r'+b) # \r prints a carriage return first, so `b` is printed on top of the previous line.
+    time.sleep(0.5)
+    if a == 4:
+        a = 0
+    if x == 20:
+        print('\033[1;32;40m Done!')
