27d7a0397d1cf777ed06571ada4fc6860eba416c
Zachery N Eckelbecker <02Zachery.eckelbecke@gulllakecs.org>
2023-01-30 14:16:22 -0500

    Added code for a random gas level and a to do list

--- TheDeclarationOfIndependence.py
+++ TheDeclarationOfIndependence.py
@@ -1,3 +1,32 @@
 # Programmer : Zachery Eckelbecker
-# Date: 1.28.2023
-# program: Infotech Center Upgrades
+# Date: 1.30.2023
+# program: Infotech Center 4.0 Gasoline
+
+"""
+We will create a Function that will tell us our fuel gauge level
+    - Create a List with Gas Levels
+    - Randomize and choose from the list to determine our gas level
+
+Create a function to determine our closest gas station
+    -Create a list of gas startions
+    -Randomly choose a gas station from the list
+
+Create a Function to determine our gas level and closest gas station
+    -Print gas level
+    -Print Closest Gas Station
+"""
+
+
+# import Libraries here
+import random
+
+
+# Gas level Function
+def gaslevelGauge():
+    gasLevelList = ["Empty","low","1/4 tank","Half Tank","3/4 Tank","Full thank"]
+    currentGasLevel = random.choice(gasLevelList)
+    return currentGasLevel
+
+
+
+print (gaslevelGauge())
