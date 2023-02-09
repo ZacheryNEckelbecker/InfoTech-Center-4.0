b849471330f2a4499de50f322db7002ad6deff9e
Zachery N Eckelbecker <02Zachery.eckelbecke@gulllakecs.org>
2023-02-09 13:52:11 -0500

    Defined weather Function and called -Stable

--- TheDeclarationOfIndependence.py
+++ TheDeclarationOfIndependence.py
@@ -1,3 +1,22 @@
 # Programmer : Zachery Eckelbecker
 # Date: 2.8.2023
 # program: Infotech Center Upgrades
+
+# import librarys here
+import random 
+
+# Create weather conditions in a list and choose them randomly 
+def weather():
+    weatherForcast = ["Snowing","Blizzard","Raining","Foggy","Windy","Icy","Sunshine"]
+    weatherCondition = random.choice(weatherForcast)
+    return weatherCondition
+
+# Variable to call weather() once our VRS
+weatherAlert = weather()
+
+print (weatherAlert)
+# VRS() to respond to the weather condtions 
+def vRS():
+    print
+
+
