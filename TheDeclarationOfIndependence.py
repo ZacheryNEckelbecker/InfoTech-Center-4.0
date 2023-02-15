0a6d2002dc98955c218068954ed0a83cda1865e3
BraydenScheffers <123008224+BraydenScheffers@users.noreply.github.com>
2023-02-15 12:50:50 -0500

    Deleted Repeated Color Code
    Stable

--- TheDeclarationOfIndependence.py
+++ TheDeclarationOfIndependence.py
@@ -25,14 +25,14 @@ a = 0
 
 while x != 20:
     x += 1
-    b = ("\033[31m[\Welcome - InfoTech 4.0 OS is Loading" + "." * a)
+    b = ("\033[31mWelcome - InfoTech 4.0 OS is Loading" + "." * a)
     a = a + 1
     sys.stdout.write('\r'+b) # \r prints a carriage return first, so `b` is printed on top of the previous line.
     time.sleep(0.25)
     if a == 4:
         a = 0
     if x == 20:
-        print('\033[31m[1;32;40m Done!')
+        print(Done!')
 
 from time import sleep
 
